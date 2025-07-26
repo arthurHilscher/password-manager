d1896e369853e34185599b0e0b8cead56d01f6c9
arthurHilscher <arthurhilscher@hotmail.de>
2025-07-26 14:19:00 +0200

    Implemented more methods for the AccountNode-class.

--- src/AccountNode.java
+++ src/AccountNode.java
@@ -13,15 +13,33 @@ public class AccountNode {
 
     public Account getAccount() {return account;}
 
+    public void setAccount(Account account) {this.account = account;}
+
     public AccountNode getParent() {return parent;}
 
     public void setParent(AccountNode parent) {this.parent = parent;}
 
     public AccountNode getLeftChild() {return leftChild;}
 
-    public void setLeftChild(AccountNode leftChild) {this.leftChild = leftChild;}
+    public void setLeftChild(AccountNode leftChild) {
+        this.leftChild = leftChild;
+        if(leftChild != null) {
+            leftChild.setParent(this);
+        }
+    }
 
     public AccountNode getRightChild() {return rightChild;}
 
-    public void setRightChild(AccountNode rightChild) {this.rightChild = rightChild;}
+    public void setRightChild(AccountNode rightChild) {
+        this.rightChild = rightChild;
+        if(rightChild != null) {
+            rightChild.setParent(this);
+        }
+    }
+
+    public boolean hasLeftChild() {return this.leftChild != null;}
+    
+    public boolean hasRightChild() {return this.rightChild != null;}
+
+    public boolean IsLeaf() {return this.leftChild == null && this.rightChild == null;}
 }
